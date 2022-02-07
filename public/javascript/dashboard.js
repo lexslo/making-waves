8c260b6f1eb4f99e97e468bc4ab4c743c780b8ea
Sparkle Party <lexi.slovik@gmail.com>
2022-02-06 20:47:31 -0800

    edit and save posts from dashboard

--- public/javascript/dashboard.js
+++ public/javascript/dashboard.js
@@ -24,10 +24,30 @@ async function postFormHandler(event) {
     }
 }
 
-async function editPostHandler() {
-    const post_id = window.location.toString().split('/')[
-        window.location.toString().split('/').length - 1
-    ];
+async function editPostHandler(e) {
+
+    // show save button and hide edit button
+    document.getElementById('save-post').classList.remove('hide');
+    document.getElementById('edit-post').classList.add('hide');
+
+    // target the post title and the post content
+    const prevPostContent = document.getElementById('post-content');
+    const prevPostTitle = document.getElementById('post-title');
+
+    prevPostContent.setAttribute('contenteditable', true);
+    prevPostTitle.setAttribute('contenteditable', true);
+
+}
+
+async function savePostHandler(e) {
+
+    const title = document.getElementById('post-title').textContent;
+    const content = document.getElementById('post-content').textContent;
+
+    const post_id = e.target.parentNode.id;
+    console.log(post_id);
+    console.log(title);
+    console.log(content);
 
     const response = await fetch('/api/posts/' + post_id, {
         method: 'put',
@@ -43,6 +63,10 @@ async function editPostHandler() {
     } else {
         alert(response.statusText);
     }
+
+    // show edit button and hide save button
+    document.getElementById('save-post').classList.add('hide');
+    document.getElementById('edit-post').classList.remove('hide');
 }
 
 async function deletePostHandler() {
@@ -64,4 +88,5 @@ async function deletePostHandler() {
 
 document.querySelector('#edit-post').addEventListener('click', editPostHandler);
 document.querySelector('#del-post').addEventListener('click', deletePostHandler);
+document.querySelector('#save-post').addEventListener('click', savePostHandler)
 document.querySelector('.post-form').addEventListener('submit', postFormHandler);
