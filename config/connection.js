22aada863e20245f4ba613d95dddd79368bf4d99
Sparkle Party <lexi.slovik@gmail.com>
2022-02-18 18:50:29 -0800

    add jawsDB statement in connection.js

--- config/connection.js
+++ config/connection.js
@@ -3,15 +3,29 @@ const Sequelize = require('sequelize');
 require('dotenv').config();
 
 const pe = process.env;
+let sequelize;
 
-const sequelize = new Sequelize(
-    pe.DB_NAME,
-    pe.DB_USER,
-    pe.DB_PW, {
-        host: 'localhost',
-        dialect: 'mysql',
-        port: 3306
-    }
-);
+// const sequelize = new Sequelize(
+//     pe.DB_NAME,
+//     pe.DB_USER,
+//     pe.DB_PW, {
+//         host: 'localhost',
+//         dialect: 'mysql',
+//         port: 3306
+//     }
+// );
+
+if (pe.JAWSDB_URL) {
+    sequelize = new Sequelize(pe.JAWSDB_URL);
+} else {
+    sequelize = new Sequelize(
+        pe.DB_NAME,
+        pe.DB_USER,
+        pe.DB_PW, {
+            host: 'localhost',
+            dialect: 'mysql',
+            port: 3306
+        });
+}
 
 module.exports = sequelize;
