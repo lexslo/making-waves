c483a1dd44efce16a79bca2a0445d1c8dc7a31fc
Sparkle Party <lexi.slovik@gmail.com>
2022-03-07 15:38:39 -0800

    remove comments

--- config/connection.js
+++ config/connection.js
@@ -5,16 +5,6 @@ require('dotenv').config();
 const pe = process.env;
 let sequelize;
 
-// const sequelize = new Sequelize(
-//     pe.DB_NAME,
-//     pe.DB_USER,
-//     pe.DB_PW, {
-//         host: 'localhost',
-//         dialect: 'mysql',
-//         port: 3306
-//     }
-// );
-
 if (pe.JAWSDB_URL) {
     sequelize = new Sequelize(pe.JAWSDB_URL);
 } else {
